5dfdff20d08e0b6c03c5a05f3c0acffddf8b7581
aws.krishan@gmail.com <aws.krishan@gmail.com>
2026-02-16 14:37:05 +0000

    station dimension adding

---
+++ models/station_dimension.sql
@@ -0,0 +1,16 @@
+with bike as(
+
+    select
+    start_statio_id as start_station_id,
+    start_station_name,
+    start_lat,
+    start_lng
+    from {{ source('demo', 'bike') }}
+
+    where ride_id != 'ride_id'
+    
+ qualify row_number() over (partition by start_statio_id order by start_statio_id desc  ) = 1
+    
+)
+
+select * from bike
